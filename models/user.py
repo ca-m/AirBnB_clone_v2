5e72fb4b50ef35496bbfd183b1524e55a9e29d9f
ca-m <deconnick.carmel@gmail.com>
2023-08-22 13:16:18 +0200

    models - user.py edit

--- models/user.py
+++ models/user.py
@@ -1,26 +1,28 @@
-""" user class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/python3
+"""Defines User class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
 class User(BaseModel, Base):
-	"""class for user
-	Attributes:
-		email: email address
-		password: password for you login
-		first_name: first name
-		last_name: last name
-	"""
-	__tablename__ = "users"
-	email = Column(String(128), nullable=False)
-	password = Column(String(128), nullable=False)
-	first_name = Column(String(128))
-	last_name = Column(String(128))
-	places = relationship("Place", cascade='all, delete, delete-orphan',
-						  backref="user")
-	reviews = relationship("Review", cascade='all, delete, delete-orphan',
-						   backref="user")
+    """Represents user for MySQL database
+
+    Attributes:
+        __tablename__ (str): name of MySQL table to store users
+        email: (sqlalchemy String): user's email address
+        password (sqlalchemy String): user's password
+        first_name (sqlalchemy String): user's first name
+        last_name (sqlalchemy String): user's last name
+        places (sqlalchemy relationship): User-Place relationship
+        reviews (sqlalchemy relationship): User-Review relationship
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
